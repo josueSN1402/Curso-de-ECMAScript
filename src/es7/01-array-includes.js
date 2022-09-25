313ab4e78a341667854f8e02a6960ce4dd491ebf
Josue Sanchez Nima <josue.sanchez.n1402@hotmail.com>
2022-09-25 17:58:05 -0500

    EcmaScript 7

---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,7 @@
+let numbers = [1, 3, 4, 6, 7, 8];
+console.log(numbers.includes(9))		// false
+
+const list = ['oscar', 'David', 'Ana']
+console.log(list.includes('oscar'))		// true
+console.log(list.includes('Oscar'))		// false
+
