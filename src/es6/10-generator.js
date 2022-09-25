0503718f66f9b7841af5f19f1dc50a283bfaa3e8
Josue Sanchez Nima <josue.sanchez.n1402@hotmail.com>
2022-09-24 19:28:37 -0500

    Generator

---
+++ src/es6/10-generator.js
@@ -0,0 +1,10 @@
+function* iterate(arr) {
+	for (let value of array) {
+		yield value;
+	}
+}
+
+const it = iterate(["Oscar", "Alexa", "David", "Gerlis"]);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
