93a93d94d04c22c7a1f30adfbea1598e61e57bd4
Josue Sanchez Nima <josue.sanchez.n1402@hotmail.com>
2022-09-29 18:58:47 -0500

    ECMAScript 9

---
+++ src/es9/03-async.js
@@ -0,0 +1,24 @@
+async function* anotherGenerator() {
+  yield await Promise.resolve(1);
+  yield await Promise.resolve(2);
+  yield await Promise.resolve(3);
+}
+
+const generador = anotherGenerator();
+generador.next().then(respuesta => console.log(respuesta.value));
+generador.next().then(respuesta => console.log(respuesta.value));
+generador.next().then(respuesta => console.log(respuesta.value));
+
+
+
+
+async function forAwait() {
+  const nombres = ["Alexa", "Oscar", "David"];
+
+  for await (let valor of nombres) {
+    console.log(valor);
+  }
+}
+
+forAwait();
+console.log("After");
