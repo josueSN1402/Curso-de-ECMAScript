095e350bdc7eba14d894357795abc73e03fdf676
Josue Sanchez Nima <josue.sanchez.n1402@hotmail.com>
2022-10-01 21:26:40 -0500

    ES12

---
+++ src/es12/01-replaceall.js
@@ -0,0 +1,8 @@
+const texto = 'Javascript es el mejor lenguaje. Javascript puede vivir en el navegador y en el servidor.'
+
+texto.replace('Javascript', 'TypeScript'); // 'TypeScript es el mejor lenguaje. Javascript puede vivir en el navegador y en el servidor.'
+
+texto.replaceAll('Javascript', 'TypeScript') // 'TypeScript es el mejor lenguaje. TypeScript puede vivir en el navegador y en el servidor.'
+
+
+// Fuente: https://platzi.com/comentario/4011055/
